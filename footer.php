45a28812518706c61217c179d1f1e7c1e73db8d8
Mehelborn <33414237+Mehelborn@users.noreply.github.com>
2019-06-05 21:55:08 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <div id = "footer">
 	<h3 align = "center" style="color:white">BookBerry:<br>
-		<a style="color:white" href="">O нашей компании</a>
+		<a style="color:white" href="page.php">O нашей компании</a>
 	</h3>
 </div>
 
