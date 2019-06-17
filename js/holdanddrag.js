0ffc848491093adf1070e6c7623b307db8457452
Mehelborn <33414237+Mehelborn@users.noreply.github.com>
2019-06-17 18:20:40 +0700

    *

--- js/holdanddrag.js
+++ js/holdanddrag.js
@@ -14,7 +14,6 @@ function dragElement(elmnt) {
   function dragMouseDown(e) {
     e = e || window.event;
     pos3 = e.clientX;
-    pos4 = e.clientY;
     document.onmouseup = closeDragElement;
     document.onmousemove = elementDrag;
   }
@@ -22,10 +21,7 @@ function dragElement(elmnt) {
   function elementDrag(e) {
     e = e || window.event;
     pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
     pos3 = e.clientX;
-    pos4 = e.clientY;
-    elmnt.style.top = elmnt.offsetTop - pos2 + "px";
     elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
   }
 
